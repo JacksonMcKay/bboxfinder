95728abaf1d04bc868fe4c9b81e56d18bc1995f9
Jackson McKay <16645707+JacksonMcKay@users.noreply.github.com>
2025-04-29 23:16:16 +1000

    Make serve script work with Python 3

--- serve.sh
+++ serve.sh
@@ -1,1 +1,6 @@
-python -m SimpleHTTPServer
+# If python3 exists, use it. else assume `python` is python2
+if command -v python3 &>/dev/null; then
+    python3 -m http.server
+else
+    python -m SimpleHTTPServer
+fi
